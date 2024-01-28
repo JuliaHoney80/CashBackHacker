0e57ddbeecd96d8a245bfa70871ad8ac69c399b7
Julia <ulia63016@gmail.com>
2024-01-28 17:13:54 +0300

    Add test in branch testng

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -3,10 +3,12 @@ package ru.netology.service;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class CashBackHackerTest {
 
     @Test
-    public void testRemain() {
+    public void testRemain1() {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
@@ -16,6 +18,69 @@ public class CashBackHackerTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testRemain2() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(expected, actual);
+    }
+    @org.testng.annotations.Test
+    public void testRemain3() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemain4() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemain5() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemain6() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+    @org.testng.annotations.Test
+    public void testRemain7() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
 }
 
 
@@ -25,4 +90,4 @@ public class CashBackHackerTest {
 
 
 
-}
+
