8bb8fea5535fd658b2adb08c4ffc26a5b925e9be
Julia <ulia63016@gmail.com>
2024-01-28 16:12:39 +0300

    change build.gradle

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -1,2 +1,28 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CashBackHackerTest {
+
+    @Test
+    public void testRemain() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+}
+
+
+
+
+
+
+
+
 }
